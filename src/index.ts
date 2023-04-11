1b8103e77f44eb0eca89c1d0f7732790634dae17
Josiah Baldwin <josiah.baldwin@gmail.com>
2023-04-10 20:30:25 -0700

    Updated to work with craiyon api v2. Maintains compatibility with v1 through second class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-import Client, { CraiyonGenerateRequestOptions } from './api/client';
+import ClientV2, {
+  CraiyonGenerateRequestOptions as CraiyonGenerateRequestOptionsV2,
+} from './api/clientv2';
+import ClientV1, {
+  CraiyonGenerateRequestOptions as CraiyonGenerateRequestOptionsV1,
+} from './api/clientv1';
+
 import CraiyonOutput from './models/CraiyonOutput';
 import CraiyonImage from './models/CraiyonImage';
 
-export { Client, CraiyonOutput, CraiyonImage, CraiyonGenerateRequestOptions };
+const Client = ClientV2;
+
+export {
+  Client,
+  ClientV1,
+  ClientV2,
+  CraiyonOutput,
+  CraiyonImage,
+  CraiyonGenerateRequestOptionsV2 as CraiyonGenerateRequestOptions,
+  CraiyonGenerateRequestOptionsV1,
+  CraiyonGenerateRequestOptionsV2,
+};
